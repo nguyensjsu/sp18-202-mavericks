f1e0c8008a8cb46606bb29c4016451ce0b2e7e3b
Kshitij Sood <kshitijsood@Kshitijs-MacBook-Pro.local>
2018-05-04 20:51:06 -0700

    new changes

--- Greenfoot-game/Rice.java
+++ Greenfoot-game/Rice.java
@@ -1,7 +1,8 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.List;
+import java.util.ArrayList;
 /**
- * Write a description of class Rice here.
+ * Write a description of class Bread here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -9,9 +10,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Rice extends items
 {
     /**
-     * Act - do whatever the Rice wants to do. This method is called whenever
+     * Act - do whatever the Bread wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Rice()
+    {
+       GreenfootImage image = getImage();
+       image.scale(50,25);
+       setImage(image);
+    }
+    public void add(items item){
+        // error operation
+    }
+    public void remove(items item){
+        // error operation
+    }  
+    public List<items> getChild(){
+      return null;   
+    }
     public void act() 
     {
         // Add your action code here.
