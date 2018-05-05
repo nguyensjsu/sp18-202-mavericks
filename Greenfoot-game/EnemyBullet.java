a070691e8b04a406c064bd4b5b2cb605198ba469
Kshitij Sood <kshitijsood@Kshitijs-MacBook-Pro.local>
2018-05-04 20:24:19 -0700

    added enemy bullet class actt

--- Greenfoot-game/EnemyBullet.java
+++ Greenfoot-game/EnemyBullet.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class EnemyBullet here.
+ * Write a description of class Bullet here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -9,11 +9,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class EnemyBullet extends Actor
 {
     /**
-     * Act - do whatever the EnemyBullet wants to do. This method is called whenever
+     * Act - do whatever the Bullet wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int speed = 0;
+    public EnemyBullet(int speed){
+        this.speed = speed;
+    }
     public void act() 
     {
         // Add your action code here.
+        World w = getWorld();
+        move(speed*5);
+        if(isAtEdge())
+        {
+            w.removeObject(this);
+        }
     }    
 }
