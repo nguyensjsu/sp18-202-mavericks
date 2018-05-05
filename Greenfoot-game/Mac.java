352866f3f982bd7507db0cd85a4dbed718defbad
Anshit Sobti <anshit@github.com>
2018-05-04 20:45:18 -0700

    New Mac class added

---
+++ Greenfoot-game/Mac.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Monkey here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mac extends roleChoice
+{
+    /**
+     * Act - do whatever the Monkey wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+   
+    public Mac(){
+      GreenfootImage image = getImage();  
+        image.scale(70, 70); 
+        setImage(image);
+    }
+    public void act() 
+    {
+        // Add your action code here.
+       
+    }    
+}
