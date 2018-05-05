be87f00fc73cc7804c78a6ba1a784ff7f5f5a55d
mshah890@gmail.com <mshah890@gmail.com>
2018-05-04 20:18:44 -0700

    Cloud.java

--- Greenfoot-game/cloud.java
+++ Greenfoot-game/cloud.java
@@ -8,12 +8,29 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class cloud extends Floor
 {
-    /**
+    /** 
      * Act - do whatever the cloud wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int speed;
+    private int right;
+    private int left;
+    public cloud(int r, int l, int s){
+       speed = s;
+       right = r;
+       left = l;
+    
+    }
+    
     public void act() 
     {
         // Add your action code here.
+        if ( (speed > 0 && getX() >= right) ||
+        (speed < 0 && getX() <=left) )
+            {
+                speed = -speed;
+      }
+        move(speed);
+       
     }    
 }
