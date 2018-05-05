5cfe5490f79fa8ca8aee8a1d832af0ed6e053a6e
mshah890@gmail.com <mshah890@gmail.com>
2018-05-04 20:42:58 -0700

    King

---
+++ Greenfoot-game/King.java
@@ -0,0 +1,69 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class tang here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class King extends Actor
+{
+    /**
+     * Act - do whatever the tang wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private SimpleTimer timer = new SimpleTimer();
+    //private int LiveTime = 1000;
+     private int LiveTime = 60000;
+     public boolean done =false;
+    public King(){
+        
+        GreenfootImage image = getImage();  
+        image.scale(60, 60); 
+        setImage(image);
+        timer.mark();
+        
+    
+    }
+    public void act() 
+    {
+        // Add your action code here.
+        if(!isTouching(Floor.class)){
+        setLocation(getX(),getY()+4);
+        turn(4);
+        }
+    if( timer.millisElapsed()>LiveTime*2){
+          removeTouching(Floor.class);
+          
+          
+        }
+        if(isAtEdge()){
+           Scenario world = (Scenario) getWorld();
+           world.GameOver();
+        }
+        if(isTouching(Player.class)){
+          SingletonBags bag = SingletonBags.getInstance();
+          if(bag.getChild().size()==2){
+              if(bag.getChild().get(0) instanceof Box){
+                   Box hasBox =(Box) bag.getChild().get(0);
+                   Shelf hasShelf = (Shelf) bag.getChild().get(1);
+                   if(hasBox.getChild().size()==2&&hasShelf.getChild().size()==3){
+                     done=true;
+                    }
+                }
+                else{
+                Box hasBox =(Box) bag.getChild().get(1);
+                   Shelf hasShelf = (Shelf) bag.getChild().get(0);
+                   if(hasBox.getChild().size()==2&&hasShelf.getChild().size()==3){
+                     done=true;
+                    }
+                }
+             
+            }
+           if(done){
+               Scenario sc = (Scenario) getWorld();
+               sc.GameFinish();
+            }
+        }
+    }    
+}
